91818d1dfa3d8b814e4673091ca82e5c935a0618
rawrex <rawrex@protonmail.ch>
2022-05-23 16:04:54 +0700

    Started to add the atomic_flag examples and usage.

--- Atomic/assign_to_nonatomic.cpp
+++ Atomic/assign_to_nonatomic.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include <atomic>
 
 int main() {
+
+	// The standard atomic types are not copyable or assignable in the conventional sense,
+	// in that they have no copy constructors or copy assignment operators.
+
+	// They do, however, support assignment from and implicit conversion to the corresponding
+	// built-in types as well as direct load() and store() member functions, exchange(), 
+	// compare_exchange_weak(), and compare_exchange_strong().
+
+	std::atomic_char atomic_ch = 'R';
+	char ch = atomic_ch; 
+
+	std::cout << ch << std::endl;
 
 }
