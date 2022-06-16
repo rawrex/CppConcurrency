df4e2e55d9c5c0e8ff484c403cab261526aadf39
rawrex <rawrex@proton.me>
2022-06-16 14:59:25 +0700

    More elaboration added to the Weak vs. Strong compare exchange.

--- Atomic/compare_exchange.cpp
+++ Atomic/compare_exchange.cpp
@@ -11,6 +11,8 @@ int main() {
 	// The return type of the compare-exchange functions is a bool,
 	// which is true if the store was performed and false otherwise.
 
+	// Weak
+	//
 	// For compare_exchange_weak(),
 	// the store might not be successful even if the original value was equal to the expected,
 	// in which case the value of the variable is unchanged and the return value of operation is false.
@@ -22,14 +24,43 @@ int main() {
 
 
 	bool expected = true;
+	// extern std::atomic<bool> b; // Set somewhere else
 	std::atomic<bool> b(false);
 		
 	JThread t1([&]()
 	{
 		// Because compare_exchange_weak() can fail spuriously, it must typically be used in a loop:
-		while(!b.compare_exchange_weak(expected,false) && !expected)
-			std::cout << "t2" << std::endl;
+		while(!b.compare_exchange_weak(expected,false) && !expected);
+
+		// Why do we need !expected check?
+		//
+		// Omitting it would yield a very similar semantics.
+		// Only in a case where another thread might reset the value to false,
+		// the semantics could become slightly different:
+		//	E.g. If the first exchange fails because it finds b is already true,
+		//	Then, with expected now true, without "&& !expected" it loops and tries another useless exchange
+		//	of true and true which may well succeed trivially breaking from the while loop,
+		//	but could exhibit meaningfully different behaviour if b had meanwhile changed back to false,
+		//	in which case the loop would continue and may ultimately set b true yet again before breaking.
 	});
+
+
+	// On platforms that no single hardware CAS instruction exist,
+	// both the weak and strong version are implemented using LL/SC (like ARM, PowerPC, etc).
+	// So is there any difference between the following two loops?
+
+	// use LL/SC (or CAS on x86) and ignore/loop on spurious failures
+	while (!compare_exchange_weak(..))		/* empty */;
+
+	// use LL/SC (or CAS on x86) and ignore/loop on spurious failures
+	while (!compare_exchange_strong(..))	/* empty */;
+
+	// E.g. We know that real LL/SC well may fail spuriously on context switch (see ./CE_LLSC_CAS.cpp).
+	// The strong would bring its own "loop" to detect a spurious failure and mask it.
+	// Note that this loop is also more complicated
+	// (it must distinguish between spurious failure and mask it, and failure due to concurrent access).
+	// ...
+
 
 	// On the other hand, compare_exchange_strong() is guaranteed to return false 
 	// only if the value wasn’t equal to the expected value.
