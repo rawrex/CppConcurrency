f1de47a3a76a5d699644b35b8bc9312458c102a4
rawrex <rawrex@proton.me>
2022-06-30 17:34:42 +0700

    Added more examples for the Sequential ordering. Started Relaxed oredering case.

--- Atomic/memory_modes.cpp
+++ Atomic/memory_modes.cpp
@@ -41,3 +41,32 @@ x.store (2);			assert (y == 1);
 // Thus, the optimizer is not free to reorder the two stores in Thread 1,
 // since Thread 2 must see the store to Y as well.
 
+
+// This also applies to loads as well:
+
+				 // Common
+				 a = 0;
+				 y = 0;
+				 b = 1;
+// Thread 1					// Thread 2
+x = a.load()				while (y.load() != b)
+y.store(b)					{}
+while (a.load() == x)		a.store(1)
+
+// Thread 2 loops until the value of 'y' changes, and proceeds to change 'a'.
+// Thread 1 is waiting for 'a' to change.
+// When normally compiling sequential code,
+// the 'while (a.load() == x)' in Thread 1 looks like an infinite loop, and might be optimized as such.
+// Instead, the load of 'a' and comparison to 'x' must happen on each iteration of the loop
+// in order for thread 1 and 2 to proceed as expected.
+
+
+// Relaxed
+//
+// The opposite approach is .
+// std::memory_order_relaxed allows for much less syncing by removing the happens-before restrictions.
+// These types of atomic operations can also have various optimizations performed on them,
+// such as dead store removal (a.k.a. DCE, dead-code elimination, is a compiler optimization
+// to remove code which does not affect the program results).
+
+
